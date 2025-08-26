ae4542833d1f02b60659d818347d1532bc0f0c5f
rizidev <166123102+rizidev@users.noreply.github.com>
2025-08-26 11:24:01 +0530

    git hub

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
-
+# This is a small change to trigger GitHub Actions
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
